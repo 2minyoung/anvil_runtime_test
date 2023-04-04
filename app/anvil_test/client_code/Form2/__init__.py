99352508f2495b73807014098aff7d5dcf36370e
LeeMinyoung <lmy-@nate.com>
2023-04-04 14:49:20 +0900

    sync code

--- app/anvil_test/client_code/Form2/__init__.py
+++ app/anvil_test/client_code/Form2/__init__.py
@@ -7,9 +7,4 @@ class Form2(Form2Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    print('Hi')
-    print('Hi')
-    print('Hi')
-    print('Hi')
-
     # Any code you write here will run before the form opens.
